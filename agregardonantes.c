11685cff11610b0e689f74b0e705dd84899f4e2b
nicolemolineros <48654860+nicolemolineros@users.noreply.github.com>
2020-03-30 18:37:49 -0500

    modifiqué la matriz

--- agregardonantes.c
+++ agregardonantes.c
@@ -3,9 +3,9 @@
 
 void agregarDonante(persona donantes[4][10], int i,int j ){
 	printf("Digite su nombre: ");
-	scanf("%s", donantes[ i ][ j ].nombre );
+	scanf("%s", donantes[ j ].nombre );
 	printf("Digite el tipo de sangre: ");
-	scanf("%s", donantes[ i ][ j ].tipo);
+	scanf("%s", donantes[ i ].tipo);
 	printf("Digite el estado: ");
 	scanf("%d", &donantes[ i++ ][ j++ ].estado);
 }
