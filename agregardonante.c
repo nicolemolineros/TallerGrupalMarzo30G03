7ff6b559e03040b18ef40738c30b4b8b77a0a821
juanpablocanas <62703709+juanpablocanas@users.noreply.github.com>
2020-03-30 17:32:16 -0500

    agregardonante.c
    Agrego la función de agregar donante

---
+++ agregardonante.c
@@ -0,0 +1,8 @@
+void agregarDonante(persona donantes[4][10], int i,int j ){
+	printf("Digite su nombre: ");
+	scanf("%s", donantes[ i ][ j ].nombre );
+	printf("Digite el tipo de sangre: ");
+	scanf("%s", donantes[ i ][ j ].tipo);
+	printf("Digite el estado: ");
+	scanf("%d", &donantes[ i++ ][ j++ ].estado);
+}
