aba38ec45cf02ed7596f18e0612c8fa1e16f4e8d
TheTenAreOne <daniel.caicedoj@gmail.com>
2020-03-31 16:21:09 -0500

    remake agregardonanates.c

--- agregardonantes.c
+++ agregardonantes.c
@@ -1,12 +1,37 @@
-
 #include "Donantes.h"
 
-void agregarDonante(persona donantes[4][10], int i,int j ){
-	printf("Digite su nombre: ");
-	scanf("%s", donantes[ i ][ j ].nombre );
-	printf("Digite el tipo de sangre: ");
-	scanf("%s", donantes[ i ][ j ].tipo);
-	printf("Digite el estado: ");
-	scanf("%d", &donantes[ i++ ][ j++ ].estado);
+void agregarDonante(persona donantes[4][10], int counter[4]){
+	
+	persona newPersona;
+	char estado;
+	int bloodType;
+	
+	printf("Nombre: "); scanf("%s", newPersona.nombre);
+	printf("Tipo de sangre: "); scanf("%s"), newPersona.tipo);
+	printf("+ o - :"); scanf("%c", &estado);
+	if(estado == '+'){
+		newPersona.estado = 0;
+	}else{
+		newPersona.estado = 1;
+	}
+	
+	if(newPersona.tipo == 'A'){
+		bloodType = 0;
+	}else if(newPersona.tipo == 'B'){
+		bloodType = 1;
+	}else if(newPersona.tipo == 'AB'){
+		bloodType = 2;
+	}else{
+		bloodType = 3;
+	}
+	
+	if(counter[bloodType] >= 10){
+		printf("No se pudo agregar donanate, arreglo lleno\n");
+		return;
+	}else{
+		donanates[bloodType][counter[bloodType]++];
+		printf(">>> {%s %c%s} agregado", newPersona.nombre, estado, newPersona.tipo);
+	}
+	
+	return;
 }
-
