57f43aba6b01ddaf40715e19b45aea565202c998
rengi0612 <rengi0612@gmail.com>
2020-03-31 19:05:40 -0500

    ajuste la cantidad de caracteres de name, corregí el if que encuentra el nombre y ajusté la impresión para que muestre el estado no como entero sino como caracter

--- banco.c
+++ banco.c
@@ -2,20 +2,25 @@
 
 void buscarDonante(persona donantes[4][10]){
   int i,j;
-  char name[20];
+  char name[25];
   
-  printf("Ingrese el nombre del donante que necesita:\n");
+  printf("Ingrese el nombre del donante que necesita: ");
   scanf("%s", name);
   for(i = 0; i < 4; i++){
     for(j = 0; j < 10; j++){
-      if(!strcmp(donantes[i][j].nombre, name) == 0){
+      if(!strcmp(donantes[i][j].nombre, name) ){
         printf("El donante %s fue encontrado exitosamente en el sistema\n", donantes[i][j].nombre);
-        printf("%s: [%s, %d]", donantes[i][j].nombre, donantes[i][j].tipo, donantes[i][j].estado);
+        printf("%s: ", donantes[i][j].nombre);
+        if (donantes[i][j].estado==0){
+          printf("[+");
+        }
+        else {printf("[-");}
+        printf("%s]", donantes[i][j].tipo);
         return;
       }   
     }
   }
-  printf("El donante %s no ha sido ingresado en el sistema", donantes[i][j].nombre); 
+  printf("El donante %s no ha sido ingresado en el sistema", name); 
   return;
 }
 
